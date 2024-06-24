cbc36e4f2533ecdd5e0554bf4cfd4503424bfbcc
Kabi-Thapa-ShresthA <ibakhtso7@gmail.com>
2024-06-24 09:51:03 +0000

    variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountID = 12546
+let accountEmail = "hitesh@google.com"
+var accountPassword = "45698"
+accountCity = "Jaipur"
+
+console.table([accountID, accountEmail, accountPassword,accountCity])
