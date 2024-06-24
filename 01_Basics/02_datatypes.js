b406b3bc765ecdb46cd07c211aec97062fe115b7
Kabi-Thapa-ShresthA <ibakhtso7@gmail.com>
2024-06-24 09:51:35 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3);
+
+
+
+console.log(typeof null);
+console.log(typeof undefined);
