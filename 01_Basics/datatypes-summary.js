6a1121636b6a3a027da81d02fd75ae6114444ee2
Kabi-Thapa-ShresthA <ibakhtso7@gmail.com>
2024-06-25 09:55:37 +0000

    stack and heap

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -3,26 +3,54 @@
 // 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
 
 //symbol
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-const bigNumber = 54546454545465454n
+// const bigNumber = 54546454545465454n
 
 
 // #Non-primitive/ Reference Type
 
 //Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"]
-let myObj = {
-    name: "hitesh",
-    age: 22,
+// const heros = ["shaktiman", "naagraj", "doga"]
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myFunction= function() {
+//     console.log("Hello World");
+// }
+
+// console.log(typeof heros);
+
+
+//************ Stack and Heap Memory*********** */
+
+//Stack memory(Primitve) variable declare gareko get a copy 
+
+// Heap Memroy(non-primitive)  original value ko reference pauxa
+let myYoutubename = "Kabi"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+let userOne = { 
+    email: "usergoogle.com",
+    upi: "user@upi"
 }
 
-const myFunction= function() {
-    console.log("Hello World");
-}
+let userTwo = userOne
 
-console.log(typeof heros);
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne);
+console.log(userTwo);
