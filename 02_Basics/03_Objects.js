cada02ad48405a4c8b4768aba441abbefc46c205
Kabi-Thapa-ShresthA <ibakhtso7@gmail.com>
2024-08-13 14:03:25 +0000

    object

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,16 @@
+// 1 singleton
+
+// object literals
+
+const jsUser = {
+    name: "Hitesh",
+    age: 18,
+    location: "jaipur",
+    email: "hitesh@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ("Monday", "Saturday")
+}
+
+console.log(jsUser.email)
+console.log(jsUser["email"])
+
