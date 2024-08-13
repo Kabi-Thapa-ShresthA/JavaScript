f9277f29ee2622d9b41f48de14b1266639b6caa8
Kabi-Thapa-ShresthA <ibakhtso7@gmail.com>
2024-08-13 14:26:51 +0000

    object one

--- 02_Basics/03_Objects.js
+++ 02_Basics/03_Objects.js
@@ -2,8 +2,12 @@
 
 // object literals
 
+const mySym = Symbol("key1")
+
 const jsUser = {
     name: "Hitesh",
+    "full name" : "Hitesh Yada",
+    [mySym] : "myKey1",
     age: 18,
     location: "jaipur",
     email: "hitesh@gmail.com",
@@ -11,6 +15,27 @@ const jsUser = {
     lastLoginDays: ("Monday", "Saturday")
 }
 
-console.log(jsUser.email)
-console.log(jsUser["email"])
+// console.log(jsUser.email)
+// console.log(jsUser["email"])
+// console.log(jsUser["full name"])
+// //console.log(typeof [mySym])
+// console.log(jsUser[mySym])
+
+jsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(jsUser)
+jsUser.email = "miscrofsot@gmail.com"
+console.log(jsUser)
+
+jsUser.greeting = function(){
+    console.log("Hello js User");
+    
+}
+
+jsUser.greetingTwo = function(){
+    console.log(`Hello js User, ${this.name}`);
+    
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
 
